8fba5543845e8f8367ab649a4c8d263b1a8ef8f7
Prashik124 <108568578+Prashik124@users.noreply.github.com>
2022-08-06 15:15:25 +0530

    Update todo_list.rb

--- todo_list.rb
+++ todo_list.rb
@@ -56,7 +56,7 @@ end
 date = Date.today
 todos = [
   { text: "Submit assignment", due_date: date - 1, completed: false },
-  { text: "Pay rent", due_date: date +1, completed: false },
+  { text: "Pay rent", due_date: date -1, completed: false },
   { text: "File taxes", due_date: date + 1, completed: false },
   { text: "Call Acme Corp.", due_date: date + 1, completed: false },
 ]
